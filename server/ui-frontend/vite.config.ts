daccbca4a904a5573db18c6ffe5a166affac219e
Vivek <siddudubey@gmail.com>
2025-12-11 14:27:53 +0000

    Normalize line endings to LF

--- server/ui-frontend/vite.config.ts
+++ server/ui-frontend/vite.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  root: 'public',
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, './src'),
+    },
+  },
   test: {
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./vitest.setup.ts'],
+    include: ['../src/**/*.test.ts'],
   },
 });
 
